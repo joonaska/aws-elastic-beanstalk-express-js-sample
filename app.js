3539d54d7eee2eb06d9afe3b971feee4fc4372c9
Joonas Kangaskoski <joonaskangaskoski@skillwells-MacBook-Pro.local>
2023-11-14 13:19:42 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Huomenta Jope!!'));
+app.get('/', (req, res) => res.send('Hyvää päivää Jope'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
